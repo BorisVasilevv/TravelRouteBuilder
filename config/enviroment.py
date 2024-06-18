d97a0de5e3fe4f8b0e7dd6cd5232c7f7a684a991
Nikita Ershov <137996541+ersnick@users.noreply.github.com>
2024-06-18 14:19:19 +0500

    Update enviroment.py

--- config/enviroment.py
+++ config/enviroment.py
@@ -1,4 +1,4 @@
 transport_url = "http://localhost:8099/api/v1/routes/on-date"
 hotel_url = "https://a0ec-213-209-148-4.ngrok-free.app/hotels/search"
-weather_url = "http://localhost:7777/weather"
+weather_url = "https://89b4-77-222-96-75.ngrok-free.app/weather"
 city_url = 'https://a6fb-2a01-540-4349-6a00-c4fa-fae2-f794-3b28.ngrok-free.app'
